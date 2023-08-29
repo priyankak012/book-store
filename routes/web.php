a3d1ed803910c1eccb6996e2a714a524e8198580
Priyanka Kediya <skeypriyankak@gmail.com>
2023-08-29 10:49:05 +0530

    connection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\RegistrationController;
+use App\Models\Registration;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use App\Models\book;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +25,65 @@ Route::get('/', function () {
 Route::get('/home',function()
 {
     return view('home');
+});
+
+Route::get('/login',function()
+{
+    return view('login');
+});
+Route::post('/login',[RegistrationController::class,'login'])->name('login');
+Route::get('/detail',function()
+{
+    return view('detail');
+});
+// Route::get('/book',function()
+// {
+
+//     return  book::all();
+//     // book::create([
+//     //     'title'=> 'technology[iit]',
+//     //     'author' => 'mk-shah',
+//     //     'description'=>"this book help to make a knowledge skill",
+//     //     'price'=>3100,
+//     //     'image' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmj7EZ-KLgcR4LYZsiJBRvdD14aTb30gqeAKP4W2KOiw&s",
+//     // ]);
+// });
+
+Route::get('/book',function()
+{
+    return view('book');
+});
+Route::post('/bookdetail',[BookController::class,'create'])->name('bookstore');
+
+Route::get('/bookdetail',function()
+{
+    return view('bookdetail');
+})->name('bookstore');
+
+
+Route::get('/registration',function()
+{
+    return view('registration');
+});
+Route::post('/registration',[RegistrationController::class,'index'])->name('storedata');
+Route::get('/index',function()
+{
+    return view('index');
+});
+Route::get('/index',[BookController::class,'show']);
+Route::post('/cart',[BookController::class,'addcart'])->name('addtocart');
+Route::get('/cart',function()
+{
+    return view('cart');
+});
+
+Route::get('/cart',function()
+{
+    Book::create([
+    'title'=>'Classics',
+    'author'=>'made by Charles Dickens',
+    'description'=>'Classics are the most read books and are even taught in schools and colleges.',
+    'price'=>2800,
+    'image'=>"public/image/",
+    ]);
 });
